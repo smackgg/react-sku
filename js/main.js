1f0e2072907fe08fe22f5f25591d5b14fd44f722
smackxu <smackxu@raventech.com>
2016-08-26 15:16:52 +0800

    Add data

--- js/main.js
+++ js/main.js
@@ -3,7 +3,9 @@ import SKUs from './skus';
 import ReactDOM from 'react-dom';
 
 // Test data
+// 商品属性数据
 import attributes from '../testData/attributes';
+// 商品库存数据
 import stocks from '../testData/stocks';
 
 // style
