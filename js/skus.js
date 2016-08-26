931dbaaa5d06cf6a04533def8f6c37bd26f17e90
smackxu <smackxu@raventech.com>
2016-08-26 15:17:48 +0800

    Modify data propTypes to array

--- js/skus.js
+++ js/skus.js
@@ -5,8 +5,8 @@ class SKUs extends React.Component {
   constructor(props) {
     super(props);
 
-    this.attributes = props.attributes.info;
-    this.stocks = props.stocks.info;
+    this.attributes = props.attributes;
+    this.stocks = props.stocks;
     this.state = {
       selectedTemp: {}, // 存放被选中的attribute
       attributes: this.attributes,
@@ -25,24 +25,24 @@ class SKUs extends React.Component {
     if (!n || n < 1) {
       return [];
     }
-    var resultArrs = [],
-      flagArr = [],
-      isEnd = false,
-      i, j, leftCnt;
-    for (i = 0; i < m; i++) {
+    const resultArrs = [];
+    const flagArr = [];
+    let isEnd = false;
+    let leftCnt;
+    for (let i = 0; i < m; i++) {
       flagArr[i] = i < n ? 1 : 0;
     }
     resultArrs.push(flagArr.concat());
     while (!isEnd) {
       leftCnt = 0;
-      for (i = 0; i < m - 1; i++) {
+      for (let i = 0; i < m - 1; i++) {
         if (flagArr[i] === 1 && flagArr[i + 1] === 0) {
-          for (j = 0; j < i; j++) {
+          for (let j = 0; j < i; j++) {
             flagArr[j] = j < leftCnt ? 1 : 0;
           }
           flagArr[i] = 0;
           flagArr[i + 1] = 1;
-          var aTmp = flagArr.concat();
+          const aTmp = flagArr.concat();
           resultArrs.push(aTmp);
           if (aTmp.slice(-n).join('').indexOf('0') === -1) {
             isEnd = true;
@@ -101,15 +101,15 @@ class SKUs extends React.Component {
           SKUResult[key].prices.push(sku.price);
         } else {
           SKUResult[key] = {
-            count : sku.count,
-            prices : [sku.price],
+            count: sku.count,
+            prices: [sku.price],
             id: [sku.id],
           };
         }
       }
       SKUResult[skuKey] = {
-        count : sku.count,
-        prices : [sku.price],
+        count: sku.count,
+        prices: [sku.price],
         id: [sku.id],
       };
     });
@@ -121,7 +121,7 @@ class SKUs extends React.Component {
     const selectedTemp = this.state.selectedTemp || {};
     const attributes = this.state.attributes;
     const skuResult = this.skuResult;
-    var nextState = {};
+    const nextState = {};
     // 根据已选中的selectedTemp，生成字典查询selectedIds
     const selectedIds = Object.keys(selectedTemp).reduce((arr, m) => {
       if (selectedTemp[m]) {
@@ -136,7 +136,7 @@ class SKUs extends React.Component {
     attributes.forEach((m) => {
       let selectedObjId;
       m.childAttr.forEach((a) => {
-        a.selected = (selectedTemp[m.title] && selectedTemp[m.title].id === a.id) ? true : false;
+        a.selected = !!(selectedTemp[m.title] && selectedTemp[m.title].id === a.id);
         if (!a.selected) {
           let testAttrIds = [];
           if (selectedTemp[m.title]) {
@@ -149,14 +149,16 @@ class SKUs extends React.Component {
           }
           testAttrIds = testAttrIds.concat(a.id);
           testAttrIds.sort((value1, value2) => parseInt(value1, 10) - parseInt(value2, 10));
-          a.unselectable = skuResult[testAttrIds.join(';')] ? false : true;
+          a.unselectable = !skuResult[testAttrIds.join(';')];
         }
       });
     });
     nextState.submitalbe = false;
     if (skuResult[selectedIds.join(';')]) {
-      const prices = skuResult[selectedIds.join(';')].prices
-      nextState.price = `${Math.max.apply(Math, prices)}~${Math.min.apply(Math, prices)}`;
+      const prices = skuResult[selectedIds.join(';')].prices;
+      const max = Math.max.apply(Math, prices);
+      const min = Math.min.apply(Math, prices);
+      nextState.price = max === min ? max : `${min}~${max}`;
       if (selectedIds.length === attributes.length) {
         nextState.submitalbe = true;
         nextState.price = skuResult[selectedIds.join(';')].prices[0];
@@ -170,6 +172,7 @@ class SKUs extends React.Component {
   clickHandler(item) {
     const attributes = this.state.attributes;
     const selectedTemp = this.state.selectedTemp;
+
     attributes.forEach((info) => {
       if (selectedTemp[info.title] && selectedTemp[info.title].id === item.id) {
         selectedTemp[info.title] = null;
@@ -239,8 +242,8 @@ class SKUs extends React.Component {
   }
 }
 SKUs.propTypes = {
-  attributes: PropTypes.object,
-  stocks: PropTypes.object,
+  attributes: PropTypes.array,
+  stocks: PropTypes.array,
 };
 
 export default SKUs;
